552ade4f3c9ad010e7fa2eee07dc775b22cb21a4
NonOxC <59771975+NonOxC@users.noreply.github.com>
2024-12-19 11:29:49 -0500

    Add files via upload

---
+++ Day3/Day3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using static System.Net.Mime.MediaTypeNames;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] lines = File.ReadAllLines("input.txt");
+        string input = string.Join("", lines);
+        string pattern = @"mul\((\d{1,3}),(\d{1,3})\)";
+        MatchCollection matches = Regex.Matches(input, pattern);
+        int total = 0;
+
+        foreach (Match match in matches)
+        {
+            int x = int.Parse(match.Groups[1].Value);
+            int y = int.Parse(match.Groups[2].Value);
+
+            total += x * y;
+        }
+
+        Console.WriteLine($"The total is: {total}");
+    }
+
+}
