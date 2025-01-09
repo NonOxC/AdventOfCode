6f0c107edd53f798ec6c6862a3eb756d754ba89b
NonOxC <59771975+NonOxC@users.noreply.github.com>
2025-01-08 19:13:51 -0500

    asdf

--- Day3/Day3/Program.cs
+++ Day3/Day3/Program.cs
@@ -11,19 +11,28 @@ class Program
     {
         string[] lines = File.ReadAllLines("input.txt");
         string input = string.Join("", lines);
-        string pattern = @"mul\((\d{1,3}),(\d{1,3})\)";
+        string pattern = @"mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don't\(\)";
         MatchCollection matches = Regex.Matches(input, pattern);
         int total = 0;
-
+        bool isEnabled = true;
         foreach (Match match in matches)
         {
-            int x = int.Parse(match.Groups[1].Value);
-            int y = int.Parse(match.Groups[2].Value);
+            if (match.Value == "do()")
+            {
+                isEnabled = true;
+            }
+            else if (match.Value == "don't()")
+            {
+                isEnabled = false;
+            }
+            else if (isEnabled && match.Groups[1].Success && match.Groups[2].Success)
+            {
+                int x = int.Parse(match.Groups[1].Value);
+                int y = int.Parse(match.Groups[2].Value);
 
-            total += x * y;
+                total += x * y;
+            }
         }
-
         Console.WriteLine($"The total is: {total}");
     }
-
 }
